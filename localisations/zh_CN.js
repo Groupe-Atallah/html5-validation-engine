511d514f2d35596e32f814ef10705902988b58ef
Corey James Lee <lee.j.corey@gmail.com>
2018-07-25 17:39:50 -0400

    feat(acq-953): Added chinese translation

---
+++ localisations/zh_CN.js
@@ -0,0 +1,20 @@
+(function() {
+    $.html5ValidationEngine.localisations.zh = {
+        required: "请填写此字段。",
+        remote: "请修复此字段。",
+        email: "请输入有效电子邮件地址。",
+        url: "请输入有效URL地址。",
+        date: "请输入有效日期。",
+        number: "请输入有效数字。",
+        digits: "请只输入数字。",
+        match : "请保持密码一致。",
+        creditcard: "请输入有效信用卡卡号。",
+        equalTo: "请再次输入相同的值。",
+        maxlength: $.html5ValidationEngine.format("请不要超过 {0} 个字符。"),
+        minlength: $.html5ValidationEngine.format("请输入至少 {0} 个字符。"),
+        minmax: $.html5ValidationEngine.format("请输入 {0} 到 {1} 个字符。"),
+        range: $.html5ValidationEngine.format("请输入 {0} 到 {1} 之间的数字。"),
+        max: $.html5ValidationEngine.format("请输入小于或等于 {0} 的数字。"),
+        min: $.html5ValidationEngine.format("请输入大于或等于 {0} 的数字。")
+    };
+}());
